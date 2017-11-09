6c2dc380721cbde30b216ab9a55625a5af217656
Driggs88 <riggs_david@hotmail.com>
2017-11-08 21:33:09 -0500

    added counter

--- src/app/my-counter/my-counter.component.ts
+++ src/app/my-counter/my-counter.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { CounterService } from '../services/counter.service';
+
 
 @Component({
   selector: 'app-my-counter',
   templateUrl: './my-counter.component.html',
   styleUrls: ['./my-counter.component.css'],
-  encapsulation: ViewEncapsulation.None
+  providers: [CounterService]
 })
-export class MyCounterComponent implements OnInit {
+  export class MyCounterComponent implements OnInit {
+  constructor(private theCounter: CounterService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  incrementCounter() {
+    this.theCounter.increment();
   }
-
 }
